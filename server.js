0f0707e779a469fec1f1e0cd0aa70281a4955f7e
AND.EVV <andrew60199@gmail.com>
2022-12-16 11:16:41 +1100

    set up the initial server

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const PORT = process.env.PORT || 3001;
+const app = express()
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+    res.json({ hello: "world" })
+})
+
+app.listen(PORT, () => {
+    console.log(`API server for THE Social Network is running on port ${PORT}!`);
+})
